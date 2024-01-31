78e7b2f66f7ce96c290016cc2854091cb9c281cc
GeraGHG <gera.GHGhernandez@gmail.com>
2024-01-31 13:43:08 -0700

    Agregando footer

--- core/settings.py
+++ core/settings.py
@@ -120,9 +120,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 MEDIA_ROOT = BASE_DIR / 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media/'
